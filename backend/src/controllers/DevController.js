11c8a805fb10e8042a8342b1c32dcc40f51cc3f0
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-01-14 16:39:35 -0300

    feat: create controllers

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(req, res) {
+        const devs = await Dev.find();
+
+        return res.json(devs);
+    },
+
+    async store(req, res) {
+        const { github_username, techs, latitude, longitude } = req.body;
+
+        try {
+            let dev = await Dev.findOne({ github_username });
+
+            if (!dev) {
+                const response = await axios.get(`https://api.github.com/users/${github_username}`);
+
+                const { name = login, avatar_url, bio } = response.data;
+
+                const techsArray = parseStringAsArray(techs);
+
+                const location = {
+                    type: 'Point',
+                    coordinates: [longitude, latitude]
+                };
+
+                dev = await Dev.create({
+                    github_username,
+                    name,
+                    avatar_url,
+                    bio,
+                    techs: techsArray,
+                    location
+                });
+
+                return res.json(dev);
+            }
+        } catch (error) {
+            return res.status(404).json('User does not exists');
+        }
+    }
+}
