13a16b5bf8540516d647b7285a262304c28e6972
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-01-21 14:51:22 -0300

    feat: connect to websocket

---
+++ findev/src/services/socket.js
@@ -0,0 +1,22 @@
+import socketio from 'socket.io-client';
+
+import credentials from '../../credentials/baseUrl';
+
+const socket = socketio(credentials.ip, { autoConnect: false });
+
+function subscribeToNewDevs(subscribeFunction) {
+    socket.on('new-dev', subscribeFunction);
+}
+
+function connect(latitude, longitude, techs) {
+    socket.io.opts.query = { latitude, longitude, techs };
+
+    socket.connect();
+}
+
+function disconnect() {
+    if (socket.connect)
+        socket.disconnect();
+}
+
+export { connect, disconnect, subscribeToNewDevs };
