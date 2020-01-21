28e0de670e268c26b98372c652ad1bcd1768337e
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-01-21 14:45:11 -0300

    feat: filter connections and technologies

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
     async index(req, res) {
@@ -36,6 +37,15 @@ module.exports = {
                     techs: techsArray,
                     location
                 });
+
+                // Filter connections that are at most 10km away
+                // and that the new dev has at least one of the filtered technologies
+                const sendSocketMessageTo = findConnections(
+                    { latitude, longitude },
+                    techsArray
+                )
+
+                sendMessage(sendSocketMessageTo, 'new-dev', dev);
 
                 return res.json(dev);
             }
